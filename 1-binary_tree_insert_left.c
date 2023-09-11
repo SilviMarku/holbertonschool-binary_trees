e346b7a9324eb2428d4465a8208b0d6d5d5b9da9
SilviMarku <marku.silvia777@gmail.com>
2023-09-11 04:47:33 -0700

    Function that inserts a node as the left-child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,30 +9,58 @@
  *@value: value to the new node being inserted
  *Return: a pointer to the created node or NULL on failure or if parent is NULL
  */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
-{
-	binary_tree_t *node = NULL;
 
-	if (parent == NULL)
-		return (NULL);
+typedef struct binary_tree_s {
+    int value;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
 
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL)
-		return (NULL);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL) {
+        return NULL;
+    }
 
-	node->n = value;
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
+    new_node->value = value;
+    new_node->parent = parent;
+    new_node->left = NULL;
+    new_node->right = NULL;
 
-	if (parent->right == NULL)
-	{
-		parent->right = node;
-		return (node);
-	}
+    return new_node;
+}
 
-	node->right = parent->right;
-	parent->right = node;
-	node->right->parent = node;
-	return (node);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) {
+    if (parent == NULL) {
+        return NULL;
+    }
+
+    binary_tree_t *new_node = binary_tree_node(parent, value);
+    if (new_node == NULL) {
+        return NULL;
+    }
+
+    if (parent->left != NULL) {
+        new_node->left = parent->left;
+        parent->left->parent = new_node;
+    }
+
+    parent->left = new_node;
+
+    return new_node;
+}
+
+int main() {
+    binary_tree_t *root = binary_tree_node(NULL, 10);
+    if (root != NULL) {
+        binary_tree_t *left_child = binary_tree_insert_left(root, 5);
+        if (left_child != NULL) {
+            printf("Left child value: %d\n", left_child->value);
+        }
+    }
+
+    free(root);
+
+    return 0;
 }
