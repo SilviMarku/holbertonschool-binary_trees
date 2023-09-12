bbbce14267239fbbee3a77aab83a3c053db4af35
SilviMarku <marku.silvia777@gmail.com>
2023-09-12 04:19:46 -0700

    Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,10 +1,26 @@
-
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - function that finds the
+ * number of leaves in a binary tree
+ * @tree: tree
+ * Return: number of leavesi
+ */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
-
+{
 	if (tree == NULL)
-		return 0;
-	
-	if (tree->left == NULL && tree->right == NULL)
-		return 
+		return (0);
 
+	else if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		size_t leaves;
+
+		leaves = binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+		return (leaves);
+	}
+}
