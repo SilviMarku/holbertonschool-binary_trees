de834a62a4af8117e632318458a2bdb7300d150a
SilviMarku <marku.silvia777@gmail.com>
2023-09-12 02:21:34 -0700

    Height

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,12 @@
+
+
+
+
+
+size_t binary_tree_sise(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	return 0;
+
+	returnn 1 + binary_tree_sise(tree->left) +
+
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_side, right_side;
 	
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	left_side = binary_tree_height(tree->left;)
+	left_side = binary_tree_height(tree->left);
 	right_side = binary_tree_height(tree->right);
 	if (left_side > right_side)
 		return(left_side + 1);
