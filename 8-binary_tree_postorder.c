bdf67c181e9a901a7beaf3108d8f21c08542681e
SilviMarku <marku.silvia777@gmail.com>
2023-09-12 02:36:36 -0700

    Postorder in binary tree

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 /**
  * binary_tree_postorder - function that goes through a binary tree using
- * pre-order traversal
+ * post-order traversal
  * @tree: pointer to the root node
  * @func: function to be appliad to all nodes traversed
  */
@@ -13,13 +13,13 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 	if (tree->left != NULL)
 	{
-		binary_tree_inorder(tree->left, func);
+		binary_tree_postorder(tree->left, func);
 		func(tree->n);
 	}
 
 	if (tree->right != NULL)
 	{
-		binary_tree_inorder(tree->right, func);
+		binary_tree_postorder(tree->right, func);
 	}
 	else
 		func(tree->n);
