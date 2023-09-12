82692fb5d9ff41b6946aca55cc17139e4461627a
SilviMarku <marku.silvia777@gmail.com>
2023-09-12 02:47:40 -0700

    Postorder in binary tree

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,14 +12,9 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	if (tree->left != NULL)
-	{
 		binary_tree_postorder(tree->left, func);
-		func(tree->n);
-	}
 
 	if (tree->right != NULL)
-	{
 		binary_tree_postorder(tree->right, func);
-	}
 		func(tree->n);
 }
