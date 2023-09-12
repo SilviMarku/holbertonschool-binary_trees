721f63ddfc1fc326cb6e26790bb7de65b237d5f5
SilviMarku <marku.silvia777@gmail.com>
2023-09-12 02:26:35 -0700

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,14 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_side, right_side;
-	
+
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	left_side = binary_tree_height(tree->left);
 	right_side = binary_tree_height(tree->right);
 	if (left_side > right_side)
-		return(left_side + 1);
+		return (left_side + 1);
 	else
 		return(right_side + 1);
 }
